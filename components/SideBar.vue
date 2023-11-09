137898fca092d5dc1ffa53574aa61b5b73455d48
mochamadgalih <mochamadglh@gmail.com>
2023-11-09 18:44:25 +0700

    sidebar + admin

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -3,12 +3,12 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <!-- <img
-        src="dist/img/AdminLTELogo.png"
+      <img
+        src="~/assets/img/AdminLTELogo.png"
         alt="AdminLTE Logo"
         class="brand-image img-circle elevation-3"
         style="opacity: 0.8"
-      /> -->
+      />
       <span class="brand-text font-weight-light">Monitoring Debit Air</span>
     </a>
 
@@ -18,7 +18,7 @@
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img
-            src="dist/img/user2-160x160.jpg"
+            src="~/assets/img/avatar.png"
             class="img-circle elevation-2"
             alt="User Image"
           />
@@ -285,64 +285,66 @@
             </ul>
           </li> -->
           <li class="nav-header">MAIN</li>
-          <a href="pages/calendar.html" class="nav-link">
-            <i class="nav-icon far fa-calendar-alt" />
-            <p>Dashboard</p>
-          </a>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt" />
+              <p>
+                Dashboard
+                <!-- <span class="badge badge-info right">2</span> -->
+              </p>
+            </a>
+          </li>
           <li class="nav-header">MASTER DATA</li>
           <li class="nav-item">
             <a href="pages/calendar.html" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt" />
-              <p>
-                Roles
-                <span class="badge badge-info right">2</span>
-              </p>
+              <i class="nav-icon fas fa-list" />
+              <p>Roles</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="pages/gallery.html" class="nav-link">
-              <i class="nav-icon far fa-image" />
+              <i class="nav-icon far fa-user" />
               User
             </a>
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-envelope" />
+              <i class="nav-icon fas fa-tag" />
               <p>
                 Water Price
-                <i class="fas fa-angle-left right" />
+                <!-- <i class="fas fa-angle-left right" /> -->
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <!-- <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="pages/mailbox/mailbox.html" class="nav-link">
                   <i class="far fa-circle nav-icon" />
-                  <p>Inbox</p>
+                  <p>Water</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="pages/mailbox/compose.html" class="nav-link">
                   <i class="far fa-circle nav-icon" />
-                  <p>Compose</p>
+                  <p>Water Depots</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="pages/mailbox/read-mail.html" class="nav-link">
                   <i class="far fa-circle nav-icon" />
-                  <p>Read</p>
+                  <p>Sensor Data</p>
                 </a>
               </li>
-            </ul>
+            </ul> -->
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-book" />
+              <i class="nav-icon fas fa-water" />
               <p>
-                Pages
-                <i class="fas fa-angle-left right" />
+                Water Usage
+                <!-- <i class="fas fa-angle-left right" /> -->
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <!-- <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="pages/examples/invoice.html" class="nav-link">
                   <i class="far fa-circle nav-icon" />
@@ -391,17 +393,17 @@
                   <p>Contacts</p>
                 </a>
               </li>
-            </ul>
+            </ul> -->
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-plus-square" />
+              <i class="nav-icon fas fa-house" />
               <p>
-                Extras
-                <i class="fas fa-angle-left right" />
+                Water Depots
+                <!-- <i class="fas fa-angle-left right" /> -->
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <!-- <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="pages/examples/login.html" class="nav-link">
                   <i class="far fa-circle nav-icon" />
@@ -474,97 +476,22 @@
                   <p>Starter Page</p>
                 </a>
               </li>
-            </ul>
-          </li>
-          <li class="nav-header">MISCELLANEOUS</li>
-          <li class="nav-item">
-            <a href="https://adminlte.io/docs/3.0" class="nav-link">
-              <i class="nav-icon fas fa-file" />
-              <p>Documentation</p>
-            </a>
-          </li>
-          <li class="nav-header">MULTI LEVEL EXAMPLE</li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="fas fa-circle nav-icon" />
-              <p>Level 1</p>
-            </a>
+            </ul> -->
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-circle" />
+              <i class="nav-icon fas fa-layer-group" />
               <p>
-                Level 1
-                <i class="right fas fa-angle-left" />
+                Sensor Data
+                <!-- <i class="fas fa-angle-left right" /> -->
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon" />
-                  <p>Level 2</p>
-                </a>
-              </li>
-              <li class="nav-item has-treeview">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon" />
-                  <p>
-                    Level 2
-                    <i class="right fas fa-angle-left" />
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon" />
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon" />
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon" />
-                      <p>Level 3</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon" />
-                  <p>Level 2</p>
-                </a>
-              </li>
-            </ul>
           </li>
+          <li class="nav-header">USER</li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="fas fa-circle nav-icon" />
-              <p>Level 1</p>
-            </a>
-          </li>
-          <li class="nav-header">LABELS</li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-danger" />
-              <p class="text">Important</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-warning" />
-              <p>Warning</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-circle text-info" />
-              <p>Informational</p>
+            <a href="https://adminlte.io/docs/3.0" class="nav-link">
+              <i class="nav-icon fas fa-right-from-bracket" />
+              <p>Log Out</p>
             </a>
           </li>
         </ul>
